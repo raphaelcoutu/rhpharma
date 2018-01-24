98101e678003710793c52fdab1b0166edbd16273
Raphaël Coutu <raph244@gmail.com>
2018-01-24 16:31:45 -0500

    Fix conversion des contraintes en disponibilité

--- app/Builders/ConstraintConverter.php
+++ app/Builders/ConstraintConverter.php
@@ -23,8 +23,25 @@ class ConstraintConverter
 
     private function convert()
     {
-        if($this->constraint->constraint_type_id == 2) {
+        $jourConstraints = collect([1,4,5,6,9,10,13,19,22,23,26]);
+        $amConstraints = collect([2,7,11,14,17,20,24,27]);
+        $pmConstraints = collect([3,8,12,15,18,21,25,28]);
+
+        $constraintId = $this->constraint->constraint_type_id;
+
+        // Indisponibilité JOUR
+        if($jourConstraints->contains($constraintId)) {
+            return ['AM' => 2, 'PM' => 2];
+        }
+
+        // Indisponibilité AM
+        else if($amConstraints->contains($constraintId)) {
             return ['AM' => 2, 'PM' => $this->actual['PM']];
+        }
+
+        // Indisponibilité PM
+        else if($pmConstraints->contains($constraintId)) {
+            return ['AM' => $this->actual['AM'], 'PM' => 2];
         }
     }
 }
