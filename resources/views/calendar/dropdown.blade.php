32a3b3c3d5c73e2d3451cd8078108b7aed78c61d
Raphaël Coutu <raph244@gmail.com>
2018-05-19 14:55:37 -0400

    Ajout vues DP et correct. USI

--- resources/views/calendar/dropdown.blade.php
+++ resources/views/calendar/dropdown.blade.php
@@ -2,6 +2,7 @@
 <li role="separator" class="divider"></li>
 <li><a href="{{ route('calendar.showByDepartment', ['schedule' => $schedule->id, 'department' => 12]) }}">C.I.M.</a></li>
 <li><a href="{{ route('calendar.showByDepartment', ['schedule' => $schedule->id, 'department' => 1]) }}">Coumadin</a></li>
+<li><a href="{{ route('calendar.showByDepartment', ['schedule' => $schedule->id, 'department' => 23]) }}">Douleur pelvienne</a></li>
 <li><a href="{{ route('calendar.showByDepartment', ['schedule' => $schedule->id, 'department' => 10]) }}">Insuffisance cardiaque</a></li>
 <li><a href="{{ route('calendar.showByDepartment', ['schedule' => $schedule->id, 'department' => 2]) }}">Médecine interne</a></li>
 <li><a href="{{ route('calendar.showByDepartment', ['schedule' => $schedule->id, 'department' => 14]) }}">Mère enfant</a></li>
@@ -10,7 +11,7 @@
 <li><a href="{{ route('calendar.showByDepartment', ['schedule' => $schedule->id, 'department' => 21]) }}">Prévoir</a></li>
 <li><a href="{{ route('calendar.showByDepartment', ['schedule' => $schedule->id, 'department' => 9]) }}">Psychiatrie</a></li>
 <li><a href="{{ route('calendar.showByDepartment', ['schedule' => $schedule->id, 'department' => 8]) }}">SIPA</a></li>
-<li><a href="{{ route('calendar.showByDepartment', ['schedule' => $schedule->id, 'department' => 4]) }}">Soins intensifs</a></li>
+<li><a href="{{ route('calendar.showByDepartment', ['schedule' => $schedule->id, 'department' => "4,5,6"]) }}">Soins intensifs</a></li>
 <li><a href="{{ route('calendar.showByDepartment', ['schedule' => $schedule->id, 'department' => 11]) }}">Soins palliatifs</a></li>
 <li><a href="{{ route('calendar.showByDepartment', ['schedule' => $schedule->id, 'department' => 16]) }}">Urgence</a></li>
 <li><a href="{{ route('calendar.showByDepartment', ['schedule' => $schedule->id, 'department' => 7]) }}">VIH</a></li>
