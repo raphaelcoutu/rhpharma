5c522d1ce46e8ac9e5327dc7a1c46509b5ec131a
Raphaël Coutu <raph244@gmail.com>
2026-09-03 22:27:54 -0400

    Fix deprecated MySQL PDO SSL constant

--- config/database.php
+++ config/database.php
@@ -1,6 +1,7 @@
 <?php
 
 use Illuminate\Support\Str;
+use Pdo\Mysql;
 
 return [
 
@@ -59,8 +60,8 @@ return [
             'strict' => true,
             'engine' => null,
             'options' => extension_loaded('pdo_mysql') ? array_filter([
-                PDO::MYSQL_ATTR_SSL_CA => env('MYSQL_ATTR_SSL_CA'),
-            ]) : [],
+                (PHP_VERSION_ID >= 80500 ? Mysql::ATTR_SSL_CA : PDO::MYSQL_ATTR_SSL_CA) => env('MYSQL_ATTR_SSL_CA'),
+            ], fn ($value) => $value !== null) : [],
         ],
 
         'pgsql' => [
