cef7d7c082e2720017e5b948ed647fe5a436bcab
Raphaël Coutu <raph244@gmail.com>
2026-09-12 13:44:19 -0400

    Use Tailwind CSS entry for React frontend

--- vite.config.js
+++ vite.config.js
@@ -6,7 +6,7 @@ import { defineConfig } from "vite";
 export default defineConfig({
     plugins: [
         laravel({
-            input: ["resources/js/app.js", "resources/sass/app.scss"],
+            input: ["resources/js/app.js", "resources/css/app.css"],
             refresh: true,
         }),
         inertia({
